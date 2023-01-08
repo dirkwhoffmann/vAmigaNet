97201f8f72e03ff1897f6e462edbd364c7b0a779
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-01-08 10:38:45 +0100

    Updated files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import "../app.css";
 	import Button from '$lib/Button.svelte'
 </script>
 
+<h1 class="text-3xl font-bold underline">
+	Hello world!
+  </h1>
+  
 <h1>Hallo to vAmiga Online</h1>
 <p><Button>Power</Button></p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the Svelte documentation</p>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
