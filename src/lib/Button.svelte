9af685a7e2dd63515538e4802c3336945575f3b6
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-01-08 13:49:37 +0100

    Uploading

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -7,16 +7,23 @@
 	export let cssVar = 'unset'
 	export let active = false
 	export let title = ""
+	export let iconName = ""
+
+	let count = 0;
+
+	function click() {
+		count += 1;
+	}
 </script>
 
 <button
 	type="button"
-	class="btn"
+	class="mx-px w-8 rounded bg-gray-700 p-1.5 text-white shadow hover:shadow-lg hover:text-gray-200 hover:scale-110"
 	class:hasIcon
 	{title}
 	{disabled}
-	on:click
-	class:active
->
+	on:click={click}
+	class:active>
+	<img class="invert" src="{iconName}.png" alt="Power Icon"/>
 	<slot />
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,46 @@
 <script lang="ts">
-	import "../app.css";
+	import "../app.css"
 	import Button from '$lib/Button.svelte'
+	import FaRegClock from 'svelte-icons/fa/FaRegClock.svelte'
 </script>
 
-<h1 class="text-3xl font-bold underline">
-	Hello world!
-  </h1>
-  
-<h1>Hallo to vAmiga Online</h1>
-<p><Button>Power</Button></p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the Svelte documentation</p>
+<body class="h-screen bg-black">
+
+<div class="flex space-x-12">
+		<div>a</div>
+		<div>a</div>
+		<div>a</div>
+</div>
+<div>
+	<Button iconName="inspectorIcon"/>
+	<Button iconName="monitorIcon"/>
+	<Button iconName="retroShellIcon"/>
+	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	<Button iconName="takeSnapshotIcon"/>
+	<Button iconName="revertSnapshotIcon"/>
+	<Button iconName="snapshotBrowserIcon"/>
+	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	<Button iconName="takeScreenshotIcon"/>
+	<Button iconName="screenshotBrowserIcon"/>
+	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	<Button iconName="portIcon"/>
+	<Button iconName="portIcon"/>
+	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	<Button iconName="keyboardIcon"/>
+	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	<Button iconName="settingsIcon"/>
+	<Button iconName="virtualMachineIcon"/>
+	&nbsp;&nbsp;&nbsp;&nbsp;
+
+	<Button iconName="pauseIcon"/>
+	<Button iconName="resetIcon"/>
+	<Button iconName="powerIcon"/>
+</div>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the Svelte documentation</p> -->
+
+</body>
