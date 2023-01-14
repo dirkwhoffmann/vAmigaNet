3cbb2a753517ef7ce840ca1aa9dabd84a535d27f
mithrendal <mithrendal@web.de>
2023-01-14 20:57:49 +0100

    wasm rentrance bug fixed

--- src/routes/emulator/+page.svelte
+++ src/routes/emulator/+page.svelte
@@ -3,28 +3,40 @@
 	import { onMount } from 'svelte';
 
 	let showMenu = false
-
 	onMount(() => {
 		console.log('onMount()');
 	});
 
 </script>
 
+<svelte:head>
+	<script type='text/javascript'>
+		console.log("do the module thing ");
+		calledRun=false;
+		Module = {
+			onRuntimeInitialized: function() {
+				  console.log("onRuntimeInitialized"); 
+			},
+			canvas: (function() { return document.getElementById('canvas'); })()
+		};
+	</script>
+	<script src="vAmiga.js"></script>
+</svelte:head>
+
 <body class="h-screen bg-black text-white">
 	<title>vAmiga Online</title>
 
 	<div>
-	{#key show}
-		<div class="aboslute z-30 fixed w-screen h-16 bg-white/30">
+		<div class="absolute z-30 fixed w-screen h-16 bg-white/30">
+			<a href="/">back</a>
+			<button on:click={()=>window['Module']._wasm_get_cpu_cycles()}>inc</button>
 		</div>
-	{/key}
 	</div>	 
 
 
 	<div class="flex justify-center absolute w-full h-full">
 		<div class="absolute z-20 w-2/3 h-2/3 bg-gray-600"> 
-			<script src="vAmiga.js"></script>
-			<canvas></canvas>
+				<canvas id="canvas" oncontextmenu="event.preventDefault()"></canvas>
 		</div>
 	</div>
 
