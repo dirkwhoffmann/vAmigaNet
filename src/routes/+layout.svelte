a660ab5c6a360c092258bdc97d5d8b75026ac9a1
mithrendal <mithrendal@web.de>
2023-01-14 21:11:49 +0100

    wasm moved to layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<script type='text/javascript'>
+		console.log("do the module thing ");
+		calledRun=false;
+		Module = {
+			onRuntimeInitialized: function() {
+				  console.log("onRuntimeInitialized"); 
+			},
+			canvas: (function() { return document.getElementById('canvas'); })()
+		};
+	</script>
+	<script src="vAmiga.js"></script>
+</svelte:head>
+<slot></slot>
--- src/routes/emulator/+page.svelte
+++ src/routes/emulator/+page.svelte
@@ -9,19 +9,7 @@
 
 </script>
 
-<svelte:head>
-	<script type='text/javascript'>
-		console.log("do the module thing ");
-		calledRun=false;
-		Module = {
-			onRuntimeInitialized: function() {
-				  console.log("onRuntimeInitialized"); 
-			},
-			canvas: (function() { return document.getElementById('canvas'); })()
-		};
-	</script>
-	<script src="vAmiga.js"></script>
-</svelte:head>
+
 
 <body class="h-screen bg-black text-white">
 	<title>vAmiga Online</title>
