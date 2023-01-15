5ddbf6f292196b6f38d35ff33a1673d32e673d6c
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-01-15 16:33:44 +0100

    checkin

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,36 @@
-#include <stdio.h>
 #include <emscripten.h>
 #include <cstdlib>
+#include <stdio.h>
 
-unsigned counter=0;
-int main()
+#include "config.h"
+#include "Amiga.h"
+
+using namespace vamiga;
+
+Amiga *amiga = nullptr;
+
+void processMsg(const void *amiga, long id, int data1, int data2, int data3, int data4)
 {
-  printf("Hello I am the vAmiga hull 3!\n");
-  return EXIT_SUCCESS;
+    printf("MSG %s: %x %x %x %x\n", MsgTypeEnum::key(id), data1, data2, data3, data4);
 }
 
-extern "C" unsigned wasm_get_cpu_cycles()
+int main(int argc, char *argv[])
 {
-  printf("wasm_get_cpu_cycles()= %u\n", counter);
-  return counter++;
+    printf("Constructing Amiga instance...\n");
+    amiga = new Amiga();
+
+    printf("Adding listener...\n");
+    amiga->msgQueue.setListener(amiga, &processMsg);
+
+    printf("Configuring...\n");
+    amiga->configure(OPT_AUDVOLL, 100);
+    amiga->configure(OPT_AUDVOLR, 100);
+    amiga->configure(OPT_AUDVOL, 0, 100);
+    amiga->configure(OPT_AUDPAN, 0, 0);
+
+    amiga->configure(OPT_CHIP_RAM, 512);
+    amiga->configure(OPT_SLOW_RAM, 512);
+    amiga->configure(OPT_AGNUS_REVISION, AGNUS_OCS);
+
+    return EXIT_SUCCESS;
 }
