9b2d94c565f627c258a6aea2d536ce94104c5e84
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-01-20 07:58:50 +0100

    Work on Button widget

--- src/lib/widgets/Button.svelte
+++ src/lib/widgets/Button.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let disabled = false;
 	export let label = '';
+	export let img = '';
 	export let pad = 'm-4';
 	export let width = 'w-max';
 </script>
@@ -8,7 +9,7 @@
 <button class="items-center" on:click>
 	<div class="bg-blue-300 hover:bg-blue-500 text-gray-800 rounded-md flex items-center h-12 w-max">
 		{#if $$slots.default}
-			<div class="justify-center h-full p-1.5 {label != '' ? '-mr-2' : ''}">
+			<div class="justify-center h-full aspect-square p-1.5 {label != '' ? '-mr-2' : ''}">
 				<slot />
 			</div>
 		{/if}
@@ -16,6 +17,9 @@
 			<div class="font-sofia-semi text-xl {pad} {width}">
 				{label}
 			</div>
+		{/if}
+		{#if img != ''}
+			<img class="flex h-full p-1.5" src={img} alt="Icon">
 		{/if}
 	</div>
 </button>
--- src/routes/emulator/+page.svelte
+++ src/routes/emulator/+page.svelte
@@ -2,10 +2,8 @@
 	import '../../app.css';
 	import { onMount } from 'svelte';
 	import Button from '$lib/widgets/Button.svelte';
-	import { vAmiga } from '$lib/stores';
 	import { goto } from '$app/navigation';
 	import RetroShell from '$lib/RetroShell.svelte';
-	import DiTerminal from 'svelte-icons/di/DiTerminal.svelte';
 	import FaAngleLeft from 'svelte-icons/fa/FaAngleLeft.svelte';
 	let showShell = true;
 
@@ -29,8 +27,8 @@
 	<div class="h-screen flex flex-col">
 		<div>
 			<div class="z-30 w-screen bg-white/30 flex space-x-2 p-2">
-				<Button on:click={goBack} label="Back"><FaAngleLeft /></Button>
-				<Button on:click={openShell}><DiTerminal /></Button>
+				<Button on:click={goBack}><FaAngleLeft /></Button>
+				<Button on:click={openShell} img="retroShellIcon.png"></Button>
 			</div>
 		</div>
 		{#if showShell}
