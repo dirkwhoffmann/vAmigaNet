6a239dcb1c4792328398cc68863a5852b0404b6b
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-01-28 20:54:40 +0100

    Update AnimatedFloat.ts

--- src/lib/utils/AnimatedFloat.ts
+++ src/lib/utils/AnimatedFloat.ts
@@ -1,34 +1,33 @@
 export class AnimatedFloat {
+	current = 0.0;
+	target = 0.0;
+	deltaStep = 0.005;
 
-    current = 0.0;
-    target = 0.0;
-    deltaStep = 0.005;
+	constructor(value = 0.0) {
+		this.current = value;
+		this.target = value;
+	}
 
-    constructor(value = 0.0) {
-        this.current = value;
-        this.target = value;
-    }
+	animates() {
+		return this.current != this.target;
+	}
 
-    animates() {
-        return this.current != this.target;
-    }
+	clamped() {
+		return this.current < 0.0 ? 0.0 : this.current > 1.0 ? 1.0 : this.current;
+	}
 
-    clamped() {
-        return this.current < 0.0 ? 0.0 : this.current > 1.0 ? 1.0 : this.current;
-    }
+	set(value: number, steps = 25) {
+		this.target = value;
+		this.deltaStep = Math.abs(this.target - this.current) / steps;
+	}
 
-    set(value: number, deltaStep = 0.005) {
-        this.target = value;
-        this.deltaStep = deltaStep;
-    }
-
-    move() {
-        if (this.current + this.deltaStep <= this.target) {
-            this.current += this.deltaStep;
-        } else if (this.current - this.deltaStep >= this.target) {            
-            this.current -= this.deltaStep;
-        } else {
-            this.current = this.target;
-        }
-    }
+	move() {
+		if (this.current + this.deltaStep <= this.target) {
+			this.current += this.deltaStep;
+		} else if (this.current - this.deltaStep >= this.target) {
+			this.current -= this.deltaStep;
+		} else {
+			this.current = this.target;
+		}
+	}
 }
