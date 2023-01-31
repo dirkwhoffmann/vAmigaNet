655675de1f176819204bfdd20d7abb596053d42c
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-01-31 12:42:39 +0100

    Fixed Safari layout bug

--- src/lib/widgets/MainPageLink.svelte
+++ src/lib/widgets/MainPageLink.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    export let href:string
+	export let href: string;
 </script>
 
-<a class="flex-col" {href}>
-    <div class="flex h-16 mt-4 justify-center text-gray-300">
-        <slot name="icon" />
-    </div>
-    <div class="flex mb-4 font-josefin text-center text-xl text-gray-300">
-        <slot name="description"/>
-    </div>
+<a class="flex flex-col items-center" {href}>
+		<div class="border-none w-20 h-20 flex mt-4 justify-center text-gray-300">
+			<slot name="icon" />
+		</div>
+		<div class="border-none flex mb-4 font-josefin text-xl text-gray-300">
+			<slot name="description" />
+		</div>
 </a>
