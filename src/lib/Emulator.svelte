513604e550def01727179cc6b7ae46a19f1f2761
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-02-08 08:03:03 +0100

    Work on layout

--- src/lib/Emulator.svelte
+++ src/lib/Emulator.svelte
@@ -18,25 +18,15 @@
 	let x2 = 0;
 	let y2 = 0;
 
-	// Emulator canvas size
-	let ww = new AnimatedFloat(0);
-	let hh = new AnimatedFloat(0);
+	// Visible texture area
+	export let textureRect = new TextureRect();
+
+	// Size of the emulator canvas
+	let recw = new AnimatedFloat(0);
+	let rech = new AnimatedFloat(0);
 
 	let www = 0;
 	let hhh = 0;
-
-	// Display size (DEPRECATED)
-	$: w = $layout == 'full' ? $canvasWidth : $layout == 'aspect' ? $aspectWidth : x2 - x1 + 2;
-	$: h = $layout == 'full' ? $canvasHeight : $layout == 'aspect' ? $aspectHeight : 2 * (y2 - y1 + 2);
-
-	$: ww.set(w);
-	$: hh.set(h);
-
-    // $: console.log('Size: ', $canvasWidth, ' x ', $canvasHeight);
-    $: console.log('wh: ', w, ' x ', h);
-
-	// The currently visible area
-	export let textureRect = new TextureRect();
 
 	onMount(() => {
 		console.log('Emulator: onMount()');
@@ -45,6 +35,26 @@
 
 		window.requestAnimationFrame(doAnimationFrame);
 	});
+
+	$: updateRect($layout);
+
+	function updateRect(layout: string) {
+		console.log('updateRect(', layout, ')');
+		if (layout == 'full') {
+			recw.set($canvasWidth);
+			rech.set($canvasHeight);
+		} else if (layout == 'aspect') {
+			recw.set($aspectWidth);
+			rech.set($aspectHeight);
+		} else if (layout == 'fit') {
+			const x1 = textureRect.x1.current * textureRect.texW;
+			const y1 = textureRect.y1.current * textureRect.texH;
+			const x2 = textureRect.x2.current * textureRect.texW;
+			const y2 = textureRect.y2.current * textureRect.texH;
+			recw.set(x2 - x1 + 2);
+			rech.set(2 * (y2 - y1 + 2));
+		}
+	}
 
 	function doAnimationFrame(now: DOMHighResTimeStamp) {
 		if (glCanvas != undefined) {
@@ -69,14 +79,18 @@
 			y1 = textureRect.y1.current * textureRect.texH;
 			x2 = textureRect.x2.current * textureRect.texW;
 			y2 = textureRect.y2.current * textureRect.texH;
-			// w = x2 - x1 + 2;
-			// h = y2 - y1 + 2;
+
+			// In 1:1 mode, use the new texture size as the emulator canvas size to get a smooth effect
+			if ($layout == 'fit') {
+				www = recw.current = recw.target = x2 - x1 + 2;
+				hhh = rech.current = rech.target = 2 * (y2 - y1 + 2);
+			}
 		}
-		if (ww.animates() || hh.animates()) {
-			ww.move();
-			hh.move();
-			www = ww.current;
-			hhh = hh.current;
+		if (recw.animates() || rech.animates()) {
+			recw.move();
+			rech.move();
+			www = recw.current;
+			hhh = rech.current;
 		}
 		glCanvas.update(now);
 	}
@@ -84,15 +98,13 @@
 	function render() {
 		glCanvas.render();
 	}
-
-
 </script>
 
 {#if $poweredOn}
 	<div class="relative grow h-full flex flex-col justify-center" transition:fade>
 		<div class="flex justify-center h-full items-center">
 			<div class="border-2 border-gray-600" style="height:{hhh}px; width:{www}px">
-                <GLCanvas bind:this={glCanvas} />
+				<GLCanvas bind:this={glCanvas} />
 			</div>
 			<!--
                 {#if $layout == 'aspect'}
