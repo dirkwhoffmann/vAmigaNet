952a72509ba8015c5cf61c1fbee85c471e3aecd4
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-02-11 15:24:19 +0100

    Work on status bar drive boxes

---
+++ src/lib/statusbar/BarBox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+export let bg = '';
+
+</script>
+
+<div class="flex h-8">
+	<div class="flex w-16 h-full justify-center items-center {bg}">
+		<slot />
+	</div>
+	<div class="h-full w-1 bg-black" />
+</div>
--- src/lib/statusbar/DeviceInfo.svelte
+++ src/lib/statusbar/DeviceInfo.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import BarBox from './BarBox.svelte';
 
 	export let cylinder = 99;
-    export let hasDisk = true;
-    export let motor = false;
-    export let writing = false;
+	export let hasDisk = true;
+	export let motor = false;
+	export let writing = false;
 
-    const gray = 'border-gray-600 border-opacity-100 bg-gradient-to-b from-gray-600 to-gray-500';
-    const green = 'border-gray-600 border-opacity-100 bg-gradient-to-b from-green-500 to-green-400';
-    const red = 'border-gray-600 border-opacity-75 opacity-100 bg-gradient-to-b from-red-500 to-red-400';
+	/*
+	const gray = 'border-gray-600 border-opacity-100 bg-gradient-to-b from-gray-600 to-gray-500';
+	const green = 'border-gray-600 border-opacity-100 bg-gradient-to-b from-green-500 to-green-400';
+	const red =
+		'border-gray-600 border-opacity-75 opacity-100 bg-gradient-to-b from-red-500 to-red-400';
+	*/
+	const gray = '';
+	const green = 'bg-gradient-to-b from-green-700 to-green-500';
+	const red = 'bg-gradient-to-b from-red-700 to-red-500';
 
-    $: src = hasDisk ? 'icons/floppy.png' : 'icons/floppy.png';
-    $: bg = motor ? (writing ? red : green) : gray;
+	$: src = hasDisk ? 'icons/floppy.png' : 'icons/floppy.png';
+	$: bg = motor ? (writing ? red : green) : gray;
 
 	// const dispatch = createEventDispatcher<{push:{sender:string}}>();
 
@@ -21,14 +28,7 @@
 	}
 </script>
 
-<div class="flex">
-	<div class="flex h-8 py-1.5 px-3">
-		<div class="border-0 h-full pr-2 flex flex-col justify-between items-center">
-			<div
-				class="h-1/2 border-[1.5px] rounded-sm w-6 {bg}"
-			/>
-			<div class="border-0 leading-none text-[7pt] text-gray-300 ">{cylinder}</div>
-		</div>
-		<img class="border-0 h-full invert opacity-50" {src} alt="Floppy icon" />
-	</div>
-</div>
+<BarBox {bg}>
+		<img class="border-0 pr-1 py-1.5 h-full invert opacity-50" {src} alt="Floppy icon" />
+		<div class="border-0 w-6 items-center pl-1 text-left leading-none text-sm text-gray-300">{cylinder}</div>
+</BarBox>
