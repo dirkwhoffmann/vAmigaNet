a53e020c6bfbd02dba90ba7a64ae9ef64bc2e223
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-02-14 17:08:08 +0100

    Playing at most three audio buffers in parallel

--- src/lib/Audio.svelte
+++ src/lib/Audio.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
+	import { proxy, amiga } from '$lib/stores';
 
-    import { proxy, amiga } from '$lib/stores';
-
-    let audioContext: AudioContext | null = null;
+	let audioContext: AudioContext | null = null;
 	let insertSound: AudioBuffer | null = null;
 	let ejectSound: AudioBuffer | null = null;
 	let stepSound: AudioBuffer | null = null;
 	let clickSound: AudioBuffer | null = null;
 
-    export async function setup() {
+	let playCnt = 0;
+
+	export async function setup() {
 		if (audioContext != null) {
 			console.log('Audio context already initialized');
-            /*
+			/*
             if (audioContext.state === 'suspended') {
 				audioContext.resume();
 			}
@@ -44,7 +45,7 @@
 
 		loadSounds();
 	}
-	
+
 	async function loadSounds() {
 		if (insertSound == null) insertSound = await loadSound('sounds/insert.mp3');
 		if (ejectSound == null) ejectSound = await loadSound('sounds/eject.mp3');
@@ -52,43 +53,40 @@
 		if (clickSound == null) clickSound = await loadSound('sounds/stephd.mp3');
 	}
 
-	export function playInsertSound() { playAudioBuffer(insertSound); }
-	export function playEjectSound() { playAudioBuffer(ejectSound); }
-	export function playStepSound() { playAudioBuffer(stepSound); }
-	export function playClickSound() { playAudioBuffer(clickSound); }
+	export function playInsertSound() {
+		playAudioBuffer(insertSound);
+	}
+	export function playEjectSound() {
+		playAudioBuffer(ejectSound);
+	}
+	export function playStepSound() {
+		playAudioBuffer(stepSound);
+	}
+	export function playClickSound() {
+		playAudioBuffer(clickSound);
+	}
 
-	async function playAudioBuffer(audio_buffer: AudioBuffer | null) {
-		if (audio_buffer == null) {
-			return;
-		}
-		/*
-		if (parallel_playing > 2 && audio_buffer == audio_df_step) {
-			//not more than 3 stepper sounds at the same time
-			return;
-		}
-		*/
-		const source = audioContext!.createBufferSource();
-		source.buffer = audio_buffer;
+	async function playAudioBuffer(buffer: AudioBuffer | null) {
+		if (buffer == null || playCnt >= 3) return;
 
-		let gain_node = audioContext!.createGain();
+		const gain_node = audioContext!.createGain();
 		gain_node.gain.value = 0.2;
 		gain_node.connect(audioContext!.destination);
 
-		source.addEventListener('ended', () => {
-			// parallel_playing--;
-		});
-
+		const source = audioContext!.createBufferSource();
+		source.buffer = buffer;
+		source.addEventListener('ended', () => { playCnt--; });
 		source.connect(gain_node);
-		// parallel_playing++;
 		source.start();
-	};
+
+		playCnt++;
+	}
 
 	async function loadSound(url: string) {
-		console.log("load_sound: url = " + url);
+		console.log('load_sound: url = ' + url);
 		let response = await fetch(url);
 		let buffer = await response.arrayBuffer();
 		let audio_buffer = await audioContext!.decodeAudioData(buffer);
 		return audio_buffer;
 	}
-
 </script>
