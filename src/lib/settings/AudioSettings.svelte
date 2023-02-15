a2196a2016829872e9de0e36bc2e3e3d74e00eb3
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-02-15 14:54:19 +0100

    Work on audio settings

--- src/lib/settings/AudioSettings.svelte
+++ src/lib/settings/AudioSettings.svelte
@@ -1,51 +1,90 @@
 <script lang="ts">
-    
-    import { onMount } from 'svelte';
-    import { proxy, amiga } from '$lib/stores';
+	import { onMount } from 'svelte';
+	import { proxy, amiga } from '$lib/stores';
 	import type { ActionEvent } from '$lib/settings/Settings.svelte';
 	import { fade } from 'svelte/transition';
 	import ConfigSection from './ConfigSection.svelte';
 	import ConfigItem from '$lib/settings/ConfigItem.svelte';
 
-    let volumeL: number;
-    let volumeR: number;
+	let volume0: number, volume1: number, volume2: number, volume3: number;
+	let volumeL: number, volumeR: number;
 
 	onMount(() => {
 		update();
 	});
 
 	function update() {
+		volume0 = $amiga.getDriveConfig($proxy.OPT_AUDVOL, 0);
+		volume1 = $amiga.getDriveConfig($proxy.OPT_AUDVOL, 1);
+		volume2 = $amiga.getDriveConfig($proxy.OPT_AUDVOL, 2);
+		volume3 = $amiga.getDriveConfig($proxy.OPT_AUDVOL, 3);
 		volumeL = $amiga.getConfig($proxy.OPT_AUDVOLL);
 		volumeR = $amiga.getConfig($proxy.OPT_AUDVOLR);
 	}
 
 	function volumeLAction(event: CustomEvent<ActionEvent>) {
-        $amiga.configure($proxy.OPT_AUDVOLL, event.detail.value);
+		$amiga.configure($proxy.OPT_AUDVOLL, event.detail.value);
 		update();
 	}
 
 	function volumeRAction(event: CustomEvent<ActionEvent>) {
-        $amiga.configure($proxy.OPT_AUDVOLR, event.detail.value);
+		$amiga.configure($proxy.OPT_AUDVOLR, event.detail.value);
 		update();
 	}
 
+	function volumeAction(event: CustomEvent<ActionEvent>) {
+		$amiga.configureDrive($proxy.OPT_AUDVOL, event.detail.tag, event.detail.value);
+		update();
+	}
 </script>
 
 <div transition:fade>
-	<ConfigSection name="Amiga Audio">
+	<ConfigSection name="Volumes">
 		<ConfigItem
-			name="Left Master Volume"
+			name="Left Speaker Volume"
 			selection={volumeL}
 			on:select={volumeLAction}
-            min={0}
-            max={100}
+			min={0}
+			max={100}
 		/>
 		<ConfigItem
-			name="Right Master Volume"
+			name="Right Speaker Volume"
 			selection={volumeR}
 			on:select={volumeRAction}
-            min={0}
-            max={100}
+			min={0}
+			max={100}
+		/>
+		<ConfigItem
+			name="Paula Channel 0 Volume"
+			selection={volume0}
+			on:select={volumeAction}
+			tag={0}
+			min={0}
+			max={100}
+		/>
+		<ConfigItem
+			name="Paula Channel 1 Volume"
+			selection={volume1}
+			on:select={volumeAction}
+			tag={1}
+			min={0}
+			max={100}
+		/>
+		<ConfigItem
+			name="Paula Channel 2 Volume"
+			selection={volume2}
+			on:select={volumeAction}
+			tag={2}
+			min={0}
+			max={100}
+		/>
+		<ConfigItem
+			name="Paula Channel 3 Volume"
+			selection={volume3}
+			on:select={volumeAction}
+			tag={3}
+			min={0}
+			max={100}
 		/>
 	</ConfigSection>
 </div>
--- src/lib/settings/ConfigItem.svelte
+++ src/lib/settings/ConfigItem.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import FaLock from 'svelte-icons/fa/FaLock.svelte';
-	import { Button, Dropdown, DropdownItem, Chevron } from 'flowbite-svelte';
 	import type { ActionEvent } from '$lib/settings/Settings.svelte';
-	import { Range, Label } from 'flowbite-svelte';
 	import { fade } from 'svelte/transition';
 
 	export let name = '???';
@@ -41,7 +39,6 @@
 		e.preventDefault();
 		dispatch('select', { tag: tag, value: e.target!.value });
 	};
-
 </script>
 
 <div class="py-0.5 px-0">
@@ -49,23 +46,20 @@
 		<div class="border-0 bg-blue-400/20 w-full h-12 flex flex-col justify-center">
 			<div class="p-4 text-xl text-blue-200 flex items-center">
 				{#if locked}
-					<div class="border-0 mr-3 h-6 w-6"><FaLock /></div>
+					<div class="border-0 mr-3 h-6 w-6 opacity-50"><FaLock /></div>
+					<div class="border-0 opacity-50">{name}</div>
+				{:else}
+					<div class="border-0">{name}</div>
 				{/if}
-				<div class="border-0">{name}</div>
 			</div>
 		</div>
 		<div class="border-0 bg-blue-400/20 h-12">
 			{#if locked}
-				<Button disabled class="!bg-transparent !text-xl !text-blue-200 flex"
-					><div class="w-64">{displayName}</div>
-					<div class="w-4 mr-2" /></Button
+				<button
+					class="btn w-[18rem] border-0 rounded-none text-xl font-normal opacity-50 text-blue-200 hover:bg-slate-600 bg-transparent"
+					>{displayName}</button
 				>
 			{:else}
-				<!--
-				<Button class="!bg-transparent !text-xl !text-blue-200"
-					><Chevron><div class="w-64">{displayName}</div></Chevron></Button
-				>
-				-->
 				<div class="dropdown dropdown-end">
 					<button
 						class="btn w-[18rem] border-0 rounded-none text-xl font-normal text-blue-200 hover:bg-slate-600 bg-transparent"
@@ -78,8 +72,8 @@
 								style="--range-shdw: none"
 								on:input={(e) => sliderAction(e)}
 								on:click={(e) => console.log('Click')}
-								min=0
-								max=100
+								min="0"
+								max="100"
 								value={selection}
 								class="range h-12 px-4 rounded-none bg-blue-200"
 							/>
