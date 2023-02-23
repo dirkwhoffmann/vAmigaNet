f36cb062c439d54a50a981ec214ac756ec41ce24
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-02-23 19:44:39 +0100

    Dropdown is closed after an item has been selected

--- src/lib/settings/ConfigItem.svelte
+++ src/lib/settings/ConfigItem.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import GoInfo from 'svelte-icons/go/GoInfo.svelte';
-	import GiPadlock from 'svelte-icons/gi/GiPadlock.svelte'
+	import GiPadlock from 'svelte-icons/gi/GiPadlock.svelte';
 	import type { ActionEvent } from '$lib/settings/Settings.svelte';
 	import { fade } from 'svelte/transition';
 	import Chevron from './Chevron.svelte';
@@ -17,7 +17,6 @@
 	export let selection = 0;
 	export let tag = 0;
 
-	let dropdownOpen = false;
 	let stepValue = 2.5;
 
 	const dispatch = createEventDispatcher<{ select: ActionEvent }>();
@@ -37,7 +36,13 @@
 
 	const handleClick = (e: MouseEvent, value: number) => {
 		e.preventDefault();
-		dropdownOpen = false;
+		const elem = document.activeElement;
+
+		// Hack to close the dropdown when an item was selected
+		if (elem) {
+			elem?.blur();
+		}
+
 		console.log('Click: id = ' + value + ' tag = ' + tag);
 		dispatch('select', { tag: tag, value: value });
 	};
@@ -57,18 +62,25 @@
 <div class="py-0.5 px-0">
 	<div class="w-full flex text-xl space-x-1 justify-between items-center">
 		<div class="border-0 bg-primary w-full h-12 flex grow overflow-hidden">
-			<div class="w-full border-0 text-xl {opac} text-primary-content flex items-center justify-between">
-				<div class="border-0 mx-2 flex grow overflow-hidden whitespace-nowrap">{name}
-				</div>
+			<div
+				class="w-full border-0 text-xl {opac} text-primary-content flex items-center justify-between"
+			>
+				<div class="border-0 mx-2 flex grow overflow-hidden whitespace-nowrap">{name}</div>
 			</div>
 		</div>
-		<div class="border-0 bg-primary w-20 h-12 flex items-center justify-center {locked ? '' : 'hidden'}">
+		<div
+			class="border-0 bg-primary w-20 h-12 flex items-center justify-center {locked
+				? ''
+				: 'hidden'}"
+		>
 			<button class="h-7 w-7 text-primary-content {opac}"><GiPadlock /></button>
 		</div>
-		<div class="border-0 bg-primary w-20 h-12 flex items-center justify-center {info ? '' : 'hidden'}">
+		<div
+			class="border-0 bg-primary w-20 h-12 flex items-center justify-center {info ? '' : 'hidden'}"
+		>
 			<button class="h-7 w-7 text-primary-content {opac}" on:click={infoAction}><GoInfo /></button>
 		</div>
-			<div class="border-0 bg-primary h-12">
+		<div class="border-0 bg-primary h-12">
 			{#if locked}
 				<button
 					class="btn btn-primary w-[18rem] border-0 rounded-none text-xl font-normal opacity-50"
@@ -76,8 +88,7 @@
 				>
 			{:else}
 				<div class="dropdown dropdown-end">
-					<button
-						class="btn btn-primary w-[18rem] border-0 rounded-none text-xl font-normal"
+					<button class="btn btn-primary w-[18rem] border-0 rounded-none text-xl font-normal"
 						><Chevron>{displayName}</Chevron>
 					</button>
 					{#if min != max}
