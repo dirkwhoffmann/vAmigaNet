7ff5b96d1e9f11a41d4980ca8d60f9107c526f4f
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-03-02 08:26:48 +0100

    cleanup

--- src/lib/DropZone/DropZone.svelte
+++ src/lib/DropZone/DropZone.svelte
@@ -3,10 +3,13 @@
 	import { Layer } from '$lib/types';
 	import { fade } from 'svelte/transition';
 	import DropZoneCell from './DropZoneCell.svelte';
+	import { audio } from '$lib/stores';
 
 	function insert(drive: number) {
+		audio.setup();
+
 		if ($dragItem) {
-			console.log("dragItem: ", dragItem);
+			console.log('dragItem: ', dragItem);
 			$amiga.insertDisk($dragItem, $dragItem.length, drive);
 		}
 		$layer = Layer.none;
@@ -15,9 +18,9 @@
 
 <div class="absolute top-10 left-0 w-screen flex justify-center" transition:fade>
 	<div class="relative flex justify-center space-x-4 p-4 rounded-xl items-center">
-			<DropZoneCell id={0} name="DF0" on:drive={ () => insert(0) } />
-			<DropZoneCell id={1} name="DF1" on:drive={ () => insert(1) } />
-			<DropZoneCell id={2} name="DF2" on:drive={ () => insert(2) } />
-			<DropZoneCell id={3} name="DF3" on:drive={ () => insert(3) } />
+		<DropZoneCell id={0} name="DF0" on:drive={() => insert(0)} />
+		<DropZoneCell id={1} name="DF1" on:drive={() => insert(1)} />
+		<DropZoneCell id={2} name="DF2" on:drive={() => insert(2)} />
+		<DropZoneCell id={3} name="DF3" on:drive={() => insert(3)} />
 	</div>
 </div>
