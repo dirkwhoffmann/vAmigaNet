5bcb731550bd26209a27a791f4cc38ec65361de4
Dirk Hoffmann <dirk.hoffmann@me.com>
2023-03-05 12:43:52 +0100

    cleanup

--- src/lib/widgets/Slider.svelte
+++ src/lib/widgets/Slider.svelte
@@ -3,27 +3,24 @@
     import type { ActionEvent } from '$lib/types';
 
     export let tag = 0;
-    export let selectedTag = 0;
+    export let value = 0;
     export let locked = false;
     export let min = 0;
     export let max = 0;
     export let open = true;
-    export let dropdownStyle = 'dropdown-bottom';
-
 
     const dispatch = createEventDispatcher<{ select: ActionEvent }>();
 
     function sliderAction(e: InputEvent)
     {
         e.preventDefault();
-        selectedTag = (e.target as HTMLInputElement).value;
-        console.log('slider: ', tag, selectedTag);
-        dispatch('select', {tag: tag, value: selectedTag});
-    };
+        value = Number((e.target as HTMLInputElement).value);
+        dispatch('select', {tag: tag, value: value});
+    }
 
 </script>
 
-<div class="dropdown {dropdownStyle}">
+<div class="dropdown dropdown-bottom">
     <!-- Make DropDown work in Safari using the label / tabindex trick (see DaisyUI doc) -->
     <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
     <!-- svelte-ignore a11y-label-has-associated-control -->
@@ -39,37 +36,9 @@
                     on:mouseup={(e) => open = false}
                     {min}
                     {max}
-                    value={selectedTag}
+                    {value}
                     class="range range-accent h-12 px-4 rounded-none"
             />
         </ul>
     {/if}
 </div>
-
-<!--
-{#if locked}
-	<button
-		class="btn btn-primary hover:bg-primary w-[18rem] border-0 rounded-none text-xl font-normal opacity-50"
-		>{selectedTag.toString()}</button
-	>
-{:else if $activeSlider == tag}
-	<ul class="menu w-[18rem] bg-primary">
-		<input
-			type="range"
-			on:input={(e) => sliderAction(e)}
-			on:click={() => console.log('Click')}
-            on:mouseup={() => console.log('mouseup')}
-			{min}
-			{max}
-			value={selectedTag}
-			class="range range-accent h-12 px-4 rounded-none"
-		/>
-	</ul>
-{:else}
-	<button
-        on:click={() => $activeSlider = tag} 
-		class="btn btn-primary w-[18rem] border-0 rounded-none text-xl font-normal"
-		>{selectedTag.toString()}
-	</button>
-{/if}
--->
